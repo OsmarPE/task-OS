ea3014a0983b9b461f32d5f5f7e320f51ec9ebc9
Osmar Perera <opb@wayakna.com>
2026-09-18 16:14:00 -0600

    styles: refactor grid

--- src/components/proyects/ProjectList.tsx
+++ src/components/proyects/ProjectList.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function ProjectList({ children }: Props) {
-  return <div className="mt-8 md:mt-4 grid gap-6 grid-cols-3 md:grid-cols-2">{children}</div>;
+  return <div className="mt-8 md:mt-4 grid gap-6 grid-cols-4 md:grid-cols-3 sm:grid-cols-2">{children}</div>;
 }
